9f058aed0ace299e06b895856af7069e6e5da352
Lduf <65276047+lduf@users.noreply.github.com>
2021-02-26 12:52:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?= "Coucou c'est un fichier index du site du CICR pour vérifier la config"?>
+<?= "Coucou c'est un fichier index du site du CICR pour vérifier la config. Qui ne semble pas marcher pour l'instant :/"?>
