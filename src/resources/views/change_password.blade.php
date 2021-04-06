194bb8a20a816e416908312d58e87b9969fc12d2
Valentina-Andreea <eteganv@gmail.com>
2021-04-06 13:54:37 +0200

    Menu for the website( plus the front of the change password page)

---
+++ src/resources/views/change_password.blade.php
@@ -0,0 +1,156 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/dbd5498a57.js" crossorigin="anonymous"></script>
+    <style>
+
+        #content {
+            position:relative;
+        }
+
+        .right-abs {
+            position:absolute;
+            top: 12px;
+            right:15px ;
+        }
+
+        h1 {
+            margin-top: 1rem;
+            text-align: center;
+        }
+
+        label {
+            display: block;
+            text-align: center;
+            font: .9rem 'Fira Sans', sans-serif;
+        }
+
+        input[type='submit' ],
+        label {
+            text-align: center;
+            margin-top: 1rem;
+        }
+
+    </style>
+    <title>Red Cross menu page</title>
+</head>
+
+<body>
+
+<!-- Top of the page with the logo,etc. >
+-->
+<div class="panel panel-default" class="fixed-top">
+    <div class="panel-body">
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgb(226, 222, 222);">
+            <h4 class="navbar-brand color:rgb(226, 222, 222); " href="#">The red Cross</h4>
+            <img src="red_cross.png" style="float:right;" alt="Red_Cross_logo" class="right-abs">
+        </nav>
+    </div>
+</div>
+
+
+
+<!-- Menu to chose the pages the we are going to access >
+ -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-light">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="nav justify-content-center nav-tabs  mr-auto; ">
+            <li class="nav-item active">
+                <a class="nav-link" href="home.blade.php"><i class="fas fa-home"></i>
+                    Home
+                    <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="analyse_data.blade.php"><i class="fas fa-server"></i>
+                    Analyse data
+                   </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="modify_data.blade.php"><i class="fas fa-mouse-pointer"></i>
+                    Modify data
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false"><i class="fas fa-user-cog"></i>
+                    Configurations
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="change_password.blade.php"><i class="fas fa-lock"></i>
+                        Change Password
+                    </a>
+                    <a class="dropdown-item" href="edit_users.blade.php"><i class="fas fa-users"></i>
+                        Edit users
+                    </a>
+                    <a class="dropdown-item" href="change_fileds.blade.php"><i class="fas fa-database"></i>
+                        Change the fields
+                    </a>
+                    <a class="dropdown-item" href="new_user.blade.php"><i class="fas fa-user-plus"></i>
+                        Create a new account
+                    </a>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+
+
+
+<!-- The page that we are curently on
+       >
+-->
+<div class="container-xxl">
+    <h1 >  Change the password </h1>
+
+    <div style="text-align:center">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+
+        <label for="pass"> New password (8 characters minimum):</label>
+        <input type="password" id="pass" name="password"
+               minlength="8" required>
+
+        <label for="pass"> Rewrite the new password :</label>
+        <input type="password" id="pass" name="password"
+               minlength="8" required>
+    </div>
+
+
+    <div style="text-align:center">
+        <input type="submit" value="Sign in">
+    </div>
+
+
+
+
+</div>
+
+
+
+<!-- Chose the language buttons  >
+-->
+<div class="panel-body">
+    <nav class="navbar ">
+        <div class="right-abs">
+            <div class="fixed-bottom">
+                <div class="btn-group" class="ribbon" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-secondary">FR</button>
+                    <button type="button" class="btn btn-secondary">EN</button>
+                    <button type="button" class="btn btn-secondary">ES</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
+
+
+</body>
+</html>
