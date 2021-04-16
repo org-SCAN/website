be01581e33bf1db1356dc4d8b407582a7257f6d5
lduf <65276047+lduf@users.noreply.github.com>
2021-04-16 15:29:18 +0200

    Ajout de la notion de Control List (il s'agit des listes telles que country role gender ...

---
+++ src/resources/views/lists_control/create.blade.php
@@ -0,0 +1,46 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Add a list
+        </h2>
+    </x-slot>
+    <div>
+        <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="mt-5 md:mt-0 md:col-span-2">
+                <form method="post" action="{{ route('lists_control.store') }}">
+                    @csrf
+                        <div class="shadow overflow-hidden sm:rounded-md">
+
+                            <!--  TITLE SECTION  -->
+                            <div class="px-4 py-5 bg-white sm:p-6">
+                                @php($form_elem = "title")
+                                <label for="{{$form_elem}}" class="block font-medium text-md text-gray-700">List's title</label>
+                                <input type="text" name="{{$form_elem}}" id="{{$form_elem}}" class="form-input rounded-md shadow-sm mt-1 block w-full" placeholder="Country" />
+                                <small id="{{$form_elem}}Help" class="block font-medium text-sm text-gray-500">It'll be shown as title when the list is used.</small>
+                                @error($form_elem)
+                                <p class="text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+                            <!--  LABEL SECTION  -->
+                            <div class="px-4 py-5 bg-white sm:p-6">
+                                @php($form_elem = "label")
+                                <label for="{{$form_elem}}" class="block font-medium text-md text-gray-700">List's label</label>
+                                <input type="text" name="{{$form_elem}}" id="{{$form_elem}}" class="form-input rounded-md shadow-sm mt-1 block w-full" placeholder="country" />
+                                <small id="{{$form_elem}}Help" class="block font-medium text-sm text-gray-500 ">It'll be used as list identifier.</small>
+                                @error($form_elem)
+                                <p class="text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+                            <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                                    Add
+                                </button>
+                            </div>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ src/resources/views/lists_control/show.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <b>{{ $list->title }}</b> details
+        </h2>
+    </x-slot>
+
+    <div>
+        <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="block mb-8">
+                <a href="{{ URL::previous()}}" class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">Back</a>
+                <a href="{{ route('lists_control.edit', $list->id) }}" class="bg-blue-200 hover:bg-blue-300 text-black font-bold py-2 px-4 rounded">Edit</a>
+            </div>
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200 w-full">
+                                <!-- TODO : display the list's content-->
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="block mt-8">
+                <a href="{{ URL::previous()}}" class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">Back</a>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
