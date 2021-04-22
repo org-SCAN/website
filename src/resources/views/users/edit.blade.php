f159a86bb1f46e68c5d5944efea770a3e981b244
Valentina-Andreea <eteganv@gmail.com>
2021-04-22 17:36:56 +0200

    1. front to edit a user(without the role of the user) 2. bugs when trying to edit( BUGS on the back-end)

--- src/resources/views/users/edit.blade.php
+++ src/resources/views/users/edit.blade.php
@@ -11,7 +11,45 @@
                 <a href="{{URL::previous() }}" class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">Back</a>
             </div>
             <div class="mt-5 md:mt-0 md:col-span-2">
+                <form method="post" action="{{ route('user.update', $user_found->id) }}">
+                    @csrf
+                    @method('put')
+                    <div class="shadow overflow-hidden sm:rounded-md">
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            <label for="name" class="block font-medium text-sm text-gray-700">Name</label>
+                            <input type="text" name="name" id="name" class="form-input rounded-md shadow-sm mt-1 block w-full"
+                                   value="{{ old('name', $user_found->name) }}" />
+                            @error('name')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                            @enderror
+                        </div>
 
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            <label for="email" class="block font-medium text-sm text-gray-700">Email</label>
+                            <input type="email" name="email" id="email" class="form-input rounded-md shadow-sm mt-1 block w-full"
+                                   value="{{ old('email', $user_found->email) }}" />
+                            @error('email')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            <label for="password" class="block font-medium text-sm text-gray-700">Password</label>
+                            <input type="password" name="password" id="password" class="form-input rounded-md shadow-sm mt-1 block w-full" />
+                            @error('password')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+
+
+                        <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
+                            <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                                Edit
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
