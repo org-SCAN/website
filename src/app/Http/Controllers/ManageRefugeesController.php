fc5d33116551bdf8c479c1d51f4d289663d356c4
Lduf <65276047+lduf@users.noreply.github.com>
2021-04-23 10:53:49 +0200

    Update ManageRefugeesController.php
    Remise en place de manageRefugees comme sur le main

--- src/app/Http/Controllers/ManageRefugeesController.php
+++ src/app/Http/Controllers/ManageRefugeesController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Field;
+use App\Http\Requests\FileRefugeeRequest;
+use App\Http\Requests\StoreRefugeeApiRequest;
 use App\Http\Requests\StoreRefugeeRequest;
 use App\Http\Requests\UpdateRefugeeRequest;
-use App\Http\Requests\StoreRefugeeApiRequest;
+use App\Models\Field;
+use App\Models\Link;
 use App\Models\Refugee;
 
 
@@ -44,6 +46,17 @@ class ManageRefugeesController extends Controller
     }
 
     /**
+     * Show the form for creating a new resource from a json file.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function createFromJson()
+    {
+        //abort_if(Gate::denies('manage_refugees_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        return view("manage_refugees.create_from_json");
+    }
+
+    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -52,7 +65,8 @@ class ManageRefugeesController extends Controller
     public function store(StoreRefugeeRequest $request)
     {
 
-       $refugee = $request->validated();
+        $refugee = $request->validated();
+
         $refugee["flight_disease"] = ((isset($refugee["flight_disease"]) && $refugee["flight_disease"] == "on") ? 1 : 0);
         $refugee["flight_boarded"] = ((isset($refugee["flight_boarded"]) && $refugee["flight_boarded"] == "on") ? 1 : 0);
 
@@ -62,23 +76,36 @@ class ManageRefugeesController extends Controller
     }
 
     /**
+     * Store a newly created resource from json file in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function storeFromJson(FileRefugeeRequest $request)
+    {
+        foreach ($request->validated() as $refugee){
+            Refugee::create($refugee);
+        }
+        return redirect()->route("manage_refugees.index");
+    }
+
+    /**
      * Display the specified resource.
      *
      * @param  String  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($user)
+    public function show(String $id)
     {
-        return view('users.show', compact('user'));
         //abort_if(Gate::denies('manage_refugees_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-       /** $fields = Field::where("status", ">", 0)
+        $fields = Field::where("status", ">", 0)
             ->orderBy("required")
             ->orderBy("order")
             ->get();
         $refugee = Refugee::find($id);
         $links = Link::where("deleted",0)->where("refugee1", $id)->orWhere("refugee2", $id)->get();
 
-        return view("manage_refugees.show", compact("refugee", "fields", "links"));*/
+        return view("manage_refugees.show", compact("refugee", "fields", "links"));
     }
 
     /**
