6538c215dc02d349f19b321e4116014472f9f6db
lduf <lucas.dufour@insa-lyon.fr>
2021-04-28 18:13:46 +0200

    Maj du design de l'index

--- src/resources/views/api_logs/index.blade.php
+++ src/resources/views/api_logs/index.blade.php
@@ -17,13 +17,13 @@
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Date
                                     </th>
-                                    <th scope="col" class="relative px-6 py-3">
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         User
                                     </th>
-                                    <th scope="col" class="relative px-6 py-3">
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Type
                                     </th>
-                                    <th scope="col" class="relative px-6 py-3">
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Action
                                     </th>
                                 </tr>
