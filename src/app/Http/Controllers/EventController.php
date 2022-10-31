780f74426ea008366abbeccaf88347e1090709bb
Lduf <lucas.dufour@insa-lyon.fr>
2022-10-31 14:17:00 +0100

    Rename basics roles to Default <role_name> add delete options for events add event to the default role permissions list
    Signed-off-by: Lduf <lucas.dufour@insa-lyon.fr>

--- src/app/Http/Controllers/EventController.php
+++ src/app/Http/Controllers/EventController.php
@@ -99,6 +99,7 @@ class EventController extends Controller
      */
     public function destroy(Event $event)
     {
-        //
+        $event->delete();
+        return redirect()->route("event.index");
     }
 }
