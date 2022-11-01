be30e7e6e4e47415f63b8185806fc4da9c571ffe
Lduf <lucas.dufour@insa-lyon.fr>
2022-11-01 13:51:33 +0100

    fix(field): Remove delete
    Remove fields delete option by forcing Policy to be false
    
    issue: #299
    Signed-off-by: Lduf <lucas.dufour@insa-lyon.fr>

--- src/app/Policies/FieldPolicy.php
+++ src/app/Policies/FieldPolicy.php
@@ -2,7 +2,10 @@
 
 namespace App\Policies;
 
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Database\Eloquent\Model;
 
 
 class FieldPolicy extends GlobalPolicy
@@ -12,5 +15,19 @@ class FieldPolicy extends GlobalPolicy
     public function __construct($route_name = "fields")
     {
         parent::__construct($route_name);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param \App\Models\User $user
+     * @param Model $model
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Model $model)
+    {
+        return false
+            ? Response::allow()
+            : Response::deny('You do not have the right to do this.');
     }
 }
