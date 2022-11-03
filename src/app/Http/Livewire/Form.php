f335ac8810edea81490afa75a6544b100924daca
AntoinePiron <pro.piron.antoine@gmail.com>
2022-11-03 11:24:49 +0100

    feat(Livewire Forms): :sparkles: add parent class Form
    #297

---
+++ src/app/Http/Livewire/Form.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Form extends Component
+{
+
+    public $form_elem;
+    public $title;
+    public $placeHolder;
+    public $hint;
+    public $previous;
+    public $warning;
+
+}
