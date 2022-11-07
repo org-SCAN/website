4019d5e09af27bf59fe43e3d583df307863d0665
Lduf <lucas.dufour@insa-lyon.fr>
2022-11-07 12:16:28 +0100

    fix(unit-test): Add one unit test
    Add at least one unit test to debug automated test on github
    
    issue: #306
    Signed-off-by: Lduf <lucas.dufour@insa-lyon.fr>

---
+++ src/tests/Unit/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * WARNING : this file has to be present else Unit directory will be ignored by .gitignore causing the automated test (on github) to fail
+     * It can be removed once there is at least one test in the Unit directory
+     */
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
