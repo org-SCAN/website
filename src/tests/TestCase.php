965d1db88cc83ea43ced8e9805ccb36a6e327b76
Lduf <lucas.dufour@insa-lyon.fr>
2022-11-10 17:48:18 +0100

    fix(feature-test): Limit memory leaks
    Try to limit memory leaks
    
    issue: #310
    Signed-off-by: Lduf <lucas.dufour@insa-lyon.fr>

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Traits\ClearProperties;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
@@ -9,6 +10,7 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use RefreshDatabase;
+    use ClearProperties;
 
     /**
      * Indicates whether the default seeder should run before each test.
@@ -16,5 +18,19 @@ abstract class TestCase extends BaseTestCase
      * @var bool
      */
     protected $seed = true;
+
+    /**
+     * Unset each property declared in this test class and its traits.
+     *
+     * @return void
+     */
+    protected function tearDown(): void {
+        parent::tearDown();
+        echo get_called_class()." -> usage :".memory_get_usage().PHP_EOL;
+        $this->clearProperties();
+        gc_collect_cycles();
+        echo get_called_class()." -> usage (after clean up) :".memory_get_usage().PHP_EOL;
+
+    }
 
 }
