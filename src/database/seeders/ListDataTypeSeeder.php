38e154fdc49d3ec730f30b8e0fe7ea6586bbbcc2
Lduf <lucas.dufour@insa-lyon.fr>
2022-11-30 12:07:51 +0100

    feat(fields/list): Add datatype as list
    - Add the data type as list (create the list and the seeder)
    - Add the visibility option to listcontrol (in DB, not yet implemented)
    - Add the datatype to list structure (in DB, not yet implemented)
    - refactor : Rename FormDoubleDropdown class name to match the file name
    
    issue: #357, #356, #359
    Signed-off-by: Lduf <lucas.dufour@insa-lyon.fr>

---
+++ src/database/seeders/ListDataTypeSeeder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Database\Seeders;
+
+class ListDataTypeSeeder extends GlobalListControlSeeder
+{
+    public function __construct(){
+        parent::__construct("ListDataType");
+    }
+}
+
