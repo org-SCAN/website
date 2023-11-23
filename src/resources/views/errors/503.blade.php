bc3c0eb4ec5349fe0f088ba4330bb5cdd406b23e
lucasdufour <lucas.dufour@insa-lyon.fr>
2023-11-23 22:57:59 +0100

    refactor(errors): Add text keys to errors views
    issue: #439
    Signed-off-by: lucasdufour <lucas.dufour@insa-lyon.fr>

--- src/resources/views/errors/503.blade.php
+++ src/resources/views/errors/503.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Service Unavailable'))
+@section('title', __('errors/errors.service_unavailable'))
 @section('code', '503')
-@section('message', __('Service Unavailable'))
+@section('message', __('errors/errors.service_unavailable'))
