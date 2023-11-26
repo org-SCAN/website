c27d32020d11133499d43cb08680e3b58204b89b
lucasdufour <lucas.dufour@insa-lyon.fr>
2023-11-26 14:12:27 +0100

    refactor(views.permissions): Add text keys to permissions views
    issue: #439
    Signed-off-by: lucasdufour <lucas.dufour@insa-lyon.fr>

---
+++ src/resources/lang/en/permissions/index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return [
+    'title' => 'Permissions',
+    'header' => 'Permissions',
+    'back_button' => 'Back',
+    'controller_name' => 'Controller Name',
+    'policy_name' => 'Policy Name',
+];
