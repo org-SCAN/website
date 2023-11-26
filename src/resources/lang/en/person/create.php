1afed895a534f6c5f102146266c139dab2bba3e5
lucasdufour <lucas.dufour@insa-lyon.fr>
2023-11-26 14:32:55 +0100

    refactor(views.person): Add text keys to person views
    issue: #439
    Signed-off-by: lucasdufour <lucas.dufour@insa-lyon.fr>

---
+++ src/resources/lang/en/person/create.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'add_item' => 'Add Item',
+    'add_an_item' => 'Add an item',
+    'back' => 'Back',
+    'add' => 'Add',
+];
---
+++ src/resources/lang/en/person/edit.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'edit_title' => 'Edit :name',
+    'back_button' => 'Back',
+    'save_button' => 'Save',
+    // Note: Add translations for dynamic field titles in your actual implementation as needed.
+];
