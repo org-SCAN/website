b6f5b1f8af1fbb4a294d44d97fbd42b74a4b64ce
lucasdufour <lucas.dufour@insa-lyon.fr>
2023-11-26 14:08:58 +0100

    refactor(views.livewire): Add text keys to livewire views
    issue: #439
    Signed-off-by: lucasdufour <lucas.dufour@insa-lyon.fr>

---
+++ src/resources/lang/en/livewire/create-form-choose-field.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    "rangeable" => [
+        "title" => "Add a range to this field ?",
+        "hint" => "If checked, you will be able to define a range for this field. Example : Age => Min : 18, Current : 20, Max : 25",
+    ],
+    "associated_list" => [
+        "title" => "Field's associated list",
+        "placeholder" => "-- Select a list --",
+        "hint" => "Define a list which is associated with this field.",
+    ],
+
+];
--- src/resources/views/livewire/forms/show/text.blade.php
+++ src/resources/views/livewire/forms/show/text.blade.php
@@ -3,23 +3,22 @@
         <div class="row">
             <div class="col-3">
                 <span class="text-gray-500">
-                    Min : {{ $field->getValue()['min'] ?? ""}}
+                    {{ __("livewire/forms/show/text.min") }} : {{ $field->getValue()['min'] ?? ""}}
                 </span>
             </div>
             <div class="col-3">
                 <span class="">
-                    Current : {{ $field->getValue()['current'] ?? ""}}
+                    {{ __("livewire/forms/show/text.current") }} : {{ $field->getValue()['current'] ?? ""}}
                 </span>
             </div>
             <div class="col-3">
                 <span class="text-gray-500">
-                    Max : {{ $field->getValue()['max'] ?? ""}}
+                    {{ __("livewire/forms/show/text.max") }} : {{ $field->getValue()['max'] ?? ""}}
                 </span>
             </div>
 
         </div>
     @else
         {{ $field->getValue() }}
-
     @endif
 </span>
