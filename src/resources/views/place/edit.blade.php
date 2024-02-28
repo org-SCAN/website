1a8109374f0c8e52417a7b08a024bcb19500128d
Antoine Piron <75019585+AntoinePiron@users.noreply.github.com>
2024-02-28 10:40:55 +0100

    use livewire forms componenet

--- src/resources/views/place/edit.blade.php
+++ src/resources/views/place/edit.blade.php
@@ -7,77 +7,75 @@
             {{ __('place/edit.edit_place', ['place_name' => $place->name]) }}
         </h2>
     </x-slot>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div>
+        <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="block mb-8">
                 <a href="{{ route('place.index') }}"
                    class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
                     {{__('common.back')}}
                 </a>
             </div>
+            <div class="mt-5 md:mt-0 md:col-span-2">
+                <form action="{{ route('place.update', $place->id) }}" method="POST"
+                      enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
 
-            <form class="w-px-500 p-3 p-md-3" action="{{ route('place.update', $place->id) }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
-                <div class="row mb-3">
-                    <label class="col-sm-3 col-form-label">{{__('place/edit.fields.name')}}</label>
-                    <div class="col-sm-9">
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
-                               placeholder="{{__('place/edit.placeholders.name')}}" value="{{$place->name}}">
-                        @error('name')
-                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
+                    <div class="shadow overflow-hidden sm:rounded-md">
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            @php($form_elem = "name")
+                            @livewire("forms.form", [
+                                'form_elem' => $form_elem,
+                                'type' => "text",
+                                'title' => __('place/create.fields.name'),
+                                'hint' => __('place/create.placeholders.name'),
+                                'previous' => $place->{$form_elem}
+                            ])
+                        </div>
 
-                <div class="row mb-3">
-                    <label class="col-sm-3 col-form-label">{{__('place/edit.fields.lat')}}</label>
-                    <div class="col-sm-9">
-                        <input type="number" step="0.000001" class="form-control @error('lat') is-invalid @enderror"
-                               name="lat" placeholder="{{__('place/edit.placeholders.lat')}}" value="{{$place->lat}}">
-                        @error('lat')
-                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                        @enderror
-                    </div>
-                </div>
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            @php($form_elem = "lat")
+                            @livewire("forms.form", [
+                                'form_elem' => $form_elem,
+                                'type' => "text",
+                                'title' => __('place/create.fields.lat'),
+                                'hint' => __('place/create.placeholders.lat'),
+                                'previous' => $place->{$form_elem}
+                            ])
+                        </div>
 
-                <div class="row mb-3">
-                    <label class="col-sm-3 col-form-label">{{__('place/edit.fields.lon')}}</label>
-                    <div class="col-sm-9">
-                        <input type="number" step="0.000001" class="form-control @error('lon') is-invalid @enderror"
-                               name="lon" placeholder="{{__('place/edit.placeholders.lon')}}" value="{{$place->lon}}">
-                        @error('lon')
-                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                        @enderror
-                    </div>
-                </div>
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            @php($form_elem = "lon")
+                            @livewire("forms.form", [
+                                'form_elem' => $form_elem,
+                                'type' => "text",
+                                'title' => __('place/create.fields.lon'),
+                                'hint' => __('place/create.placeholders.lon'),
+                                'previous' => $place->{$form_elem}
+                            ])
+                        </div>
 
-                <div class="row mb-3">
-                    <label class="col-sm-3 col-form-label">{{__('place/edit.fields.description')}}</label>
-                    <div class="col-sm-9">
-                        <input type="text" class="form-control @error('description') is-invalid @enderror"
-                               name="description" placeholder="{{__('place/edit.placeholders.description')}}" value="{{$place->description}}">
-                        @error('description')
-                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                        @enderror
-                    </div>
-                </div>
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            @php($form_elem = "description")
+                            @livewire("forms.form", [
+                                'form_elem' => $form_elem,
+                                'type' => "text",
+                                'title' => __('place/create.fields.description'),
+                                'hint' => __('place/create.placeholders.description'),
+                                'previous' => $place->{$form_elem}
+                            ])
+                        </div>
 
-                <div class="row mb-3">
-                    <label class="col-sm-3 col-form-label"></label>
-                    <div class="col-sm-9">
-                        <button type="submit" class="btn btn-success btn-block text-white">{{__('common.save')}}</button>
+                        <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
+                            <button
+                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                                {{ __('common.save') }}
+                            </button>
+                        </div>
+
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
 </x-app-layout>
