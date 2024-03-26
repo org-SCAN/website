2a2b6bd0e8fc66c138a0005e4f2c0bf47e45c4a3
vqlion <valentin.jossic@insa-lyon.fr>
2024-03-26 18:00:47 +0100

    feat(MatchingAlgorithm):
    Add match algorithms list and seeder
    
    issue: #479

---
+++ src/app/Models/ListMatchingAlgorithm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ListMatchingAlgorithm extends ListControl
+{
+
+}
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ListRelationSeeder::class);
         $this->call(ListEventTypeSeeder::class);
         $this->call(ListSourceTypeSeeder::class);
+        $this->call(ListMatchingAlgorithmSeeder::class);
         if (env('APP_DEBUG') && env('APP_ENV') == 'local') {
             $this->call(EventSeeder::class);
             $this->call(SourceSeeder::class);
