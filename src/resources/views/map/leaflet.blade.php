1a33d4b2b79936a589b670d56e78aa1dba3566ad
rebeccadjim <rebecca.djimtoingar-taroum@insa-lyon.fr>
2024-07-03 21:22:26 +0200

    feat(Places): integrity
    issue: #494

--- src/resources/views/map/leaflet.blade.php
+++ src/resources/views/map/leaflet.blade.php
@@ -14,13 +14,12 @@
             height: 400px;
         }
     </style>
-    <link rel='stylesheet' href='https://unpkg.com/leaflet@1.8.0/dist/leaflet.css' crossorigin='' />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 </head>
 
 <body>
 <div id='map'></div>
-
-<script src='https://unpkg.com/leaflet@1.8.0/dist/leaflet.js' crossorigin=''></script>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 <script>
     let map, markers = [];
 
